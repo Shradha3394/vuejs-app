afe15b9b1903a1cb8333d2e54976181465a0aac4
Shradha <sjoshi@ex2india.com>
2018-12-23 01:13:24 +0530

    Vue demo app

---
+++ src/components/ProductCategory.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container-fluid">
+        <div class="row my-2" v-for="category in categories" v-bind:key="category">
+            <button class="btn btn-block" v-bind:class="category == currentCategory ? 'btn-primary' : 'btn-secondary'" v-on:click="setCategory(category)">
+                {{ category }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { mapState, mapGetters, mapMutations } from "vuex";
+
+    export default {
+        computed:
+        {
+            ...mapState(["currentCategory"]),
+            ...mapGetters(["categories"])
+        },
+        methods: {
+            ...mapMutations(["setCategory"])
+        }
+    }
+</script>
---
+++ src/components/Store.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col bg-dark text-white">
+                <a class="navbar-brand">SPORTS STORE</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-info p-2">
+                <ProductCategory />
+            </div>
+            <div class="col-9 p-2 ">
+                <product-list />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ProductList from './ProductList.vue'
+    import ProductCategory from './ProductCategory.vue'
+
+    export default {
+        components: {
+            ProductList,
+            ProductCategory
+        }
+    }
+</script>
